34be797d8fa0ea29e4663bcd2397e6d7fc80147c
Alena Nikitina <alena.tkachenk@mail.ru>
2023-09-07 21:01:34 +0300

    adapniv in warning done

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,21 +1,18 @@
-import React from "react";
+import React from 'react';
 import Header from '../components/Header/Header';
 import Promo from './Promo/Promo';
 import AboutMe from './AboutMe/AboutMe';
-//import Service from './Service/Service';
 import Footer from './Footer/Footer';
 import Ad from './Ad/Ad';
 import Stage from './Stage/Stage';
-import Warning from "./Warning/Warning";
+import Warning from './Warning/Warning';
+import Consultation from './Consultation/Consultation';
 
-import Consultation from "./Consultation/Consultation";
-
-//import { Route, Routes } from "react-router-dom";
+//import { Route, Routes } from 'react-router-dom';
 
 export default function App() {
-  
   return (
-    <div className="App page">
+    <div className='App page'>
       <Header />
       <Promo />
       <AboutMe />
@@ -25,5 +22,5 @@ export default function App() {
       <Consultation />
       <Footer />
     </div>
-  );
+  )
 }
