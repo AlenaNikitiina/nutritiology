db3ca44ecd035b57f2ed6806b672b8676174c615
Alena Nikitina <alena.tkachenk@mail.ru>
2024-03-29 11:54:51 +0200

    fix add favicons

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 import './Footer.css';
 //import vk2 from '../../images/vk2.svg';
-import v from '../../images/v.svg';
+//import v from '../../images/v.svg';
 import telegram from '../../images/telegram.svg';
 
 
@@ -18,7 +18,7 @@ export default function Footer () {
           <ul className="footer__nav-links">
             <li>
               <a href="#" className="footer__nav-link link" target="blank">
-                <img className="footer__social-icon" src={v} alt="Вконтакте" />Vkontakte</a>
+                <img className="footer__social-icon" src={telegram} alt="Вконтакте" />Vkontakte</a>
             </li>
             <li>
               <a href="tg://resolve?domain=litueva_life" className="footer__nav-link link" target="blank">
